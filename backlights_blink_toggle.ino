bf8f0528bd4d80bfde2c7777dfa191e01f122c08
Jacob Kolb <jkolb12@gmail.com>
2017-06-30 17:02:50 -0600

    Add files via upload

---
+++ backlights_blink_toggle.ino
@@ -0,0 +1,19 @@
+//By Jacob Kolb
+//simple blink sketch loaded on to a 5v Adafruit Trinket 
+
+//led & button pins
+const int blinkerLED = 2;
+//timing & delays
+int wait = 500;
+
+void setup() {
+  pinMode(blinkerLED,OUTPUT);
+}
+
+void loop() {
+  digitalWrite(blinkerLED,HIGH);
+  delay(wait);
+  digitalWrite(blinkerLED,LOW);
+  delay(wait);
+  
+}
